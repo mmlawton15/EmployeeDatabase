2039ff478db5594a745d917b5b41ad4a1bef2296
Mary Margaret <mmlawton15@gmail.com>
2022-01-26 10:56:20 -0500

    added the prompts for the sql code to be generated

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // DONE - GIVEN a command-line application that accepts user input
 // DONE - WHEN I start the application
 // DONE - THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-// WHEN I choose to view all departments
+// DONE - WHEN I choose to view all departments
 // THEN I am presented with a formatted table showing department names and department ids
 // WHEN I choose to view all roles
 // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
@@ -23,7 +23,7 @@ const fs = require('fs');
 //prompt user for what they want to do
 const promptUserForStep = () => {
     return inquirer.prompt({
-            name:'hello!',
+            name:'step',
             type:'list',
             message:'Welcome to the Employee Database. What would you like to do?',
             choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a role', 'Add an Employee', 'Update Employee Role']
@@ -33,5 +33,25 @@ const promptUserForStep = () => {
 //may be junmping gun here, what to do with that data from above
 promptUserForStep()
 .then((data) => {
+    if (data.step === 'View All Departments') {
 
+    }
+    if (data.step === 'View All Roles') {
+
+    }
+    if (data.step === 'View All Employees') {
+
+    }
+    if (data.step === 'Add a Department') {
+
+    }
+    if (data.step === 'Add a Role') {
+
+    }
+    if (data.step === 'Add an Employee') {
+
+    }
+    if (data.step === 'Update Employee Role') {
+
+    }
 })
